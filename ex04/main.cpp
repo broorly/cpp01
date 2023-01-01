ebe7b67f227d967cdac94b0677e1bc41b35fb9f4
Mohamed-amine Rafik <mrafik@e2r4p4.1337.ma>
2023-01-01 21:49:47 +0100

    fixed ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,23 +1,38 @@
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-using namespace std;
+int main(int argc, char **argv) {
+  std::ifstream infile;
+  std::stringstream buffer;
+  std::ofstream outfile;
+  std::string text;
 
-int main() {
-  ifstream inputFile;
-  inputFile.open("numbers.txt");
-
-  if (!inputFile.is_open()) {
-    cerr << "Error opening file" << endl;
-    return 1;
+  if (argc != 4) 
+  {
+    std::cout << "invalid argument !!" << std::endl;
+    exit(1);
   }
-
-  int x;
-  while (inputFile >> x) {
-    cout << x << endl;
+  infile.open(argv[1]);
+  if (infile.fail()) 
+  {
+    std::cout << "Error opening file!" << std::endl;
+    exit(1);
   }
-
-  inputFile.close();
-
-  return 0;
+  buffer << infile.rdbuf();
+  infile.close();
+  text = buffer.str();
+  int i = 0;
+  while ((i = text.find((std::string)argv[2], i)) != -1)
+  {
+    text.erase(i, ((std::string)argv[2]).length());
+    text.insert(i, (std::string)argv[3]);
+    i += ((std::string)argv[3]).length();
+  }
+  outfile.open(argv[1] + (std::string) ".replace");
+  outfile << text;
+  outfile.close();
+  return (0);
 }
